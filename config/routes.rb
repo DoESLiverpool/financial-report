13ab052d7a58fd17331bdafcf88554d44cdc272b
John McKerrell <john@mckerrell.net>
2019-11-06 20:08:40 +0000

    Upgrade to Rails 6.0.1

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get "/reports/categories", to: "reports#categories"
   get "/reports/income_distribution", to: "reports#income_distribution"
   get "/reports/service_users", to: "reports#service_users"
